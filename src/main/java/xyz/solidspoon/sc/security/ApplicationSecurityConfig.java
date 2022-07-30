180e0762e795cfc102ce19d40af24737861a3191
solidspoon <cedarrival@outlook.com>
2022-07-30 17:27:22 +0800

    customize remember-me expire time

--- src/main/java/xyz/solidspoon/sc/security/ApplicationSecurityConfig.java
+++ src/main/java/xyz/solidspoon/sc/security/ApplicationSecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import java.util.concurrent.TimeUnit;
+
 import static xyz.solidspoon.sc.security.ApplicationUserPermeation.COURSE_WRITE;
 import static xyz.solidspoon.sc.security.ApplicationUserRole.*;
 
@@ -46,7 +48,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/courses", true)
                 .and()
-                .rememberMe(); // change expiration time from 20 minutes up to 2 weeks by default
+                .rememberMe()
+                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21)) // change expiration time from 20 minutes up to 2 weeks by default
+                .key("somethingverysecurd");
     }
 
     @Override
