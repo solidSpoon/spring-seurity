bd27ac190d68d308fccd3bfa44ebce6d57c2dfef
solidspoon <cedarrival@outlook.com>
2022-07-30 18:03:12 +0800

    customize logout api

--- src/main/java/xyz/solidspoon/sc/security/ApplicationSecurityConfig.java
+++ src/main/java/xyz/solidspoon/sc/security/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -50,7 +51,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .rememberMe()
                 .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21)) // change expiration time from 20 minutes up to 2 weeks by default
-                .key("somethingverysecurd");
+                .key("somethingverysecurd")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                // when enable csrf, should use POST
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID","remember-me")
+                .logoutSuccessUrl("/login");
     }
 
     @Override
